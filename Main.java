fa1514b2162e6315099631ad3226c4230d85db44
fiscn <fiscn2709@wrdsb.ca>
2020-10-01 15:30:53 +0000

    Finished A3 Q1 program "Maximum integer"

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * a program that will find the maximum integer between three numbers that the user inputs.
+ * @author Neil Fischer 
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create Scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    // prompt the user to enter three integers 
+    System.out.println("Please enter three integers on separate lines."); 
+
+    // declaring variables
+    int Number1 = input.nextInt(); 
+    int Number2 = input.nextInt(); 
+    int Number3 = input.nextInt(); 
+
+    // find out which integer is the greatest 
+    if (Number1 >= Number2 && Number1 >= Number3){
+      System.out.println("Maximum: " + Number1); 
+    }else if (Number2 >= Number1 && Number2 >= Number3){
+      System.out.println("Maximum: " + Number2);
+    }else if (Number3 >= Number1 && Number3 >= Number2){
+      System.out.println("Maximum: " + Number3);
+    }
+
+
     
   }
 }
